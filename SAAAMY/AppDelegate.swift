11aeb61b44fbe99a42b86562997c0c1f09879d89
Samy Ismail <samyismail@samys-mbp.attlocal.net>
2018-05-02 15:54:55 -0400

    Samy's addition

--- SAAAMY/AppDelegate.swift
+++ SAAAMY/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     //IDK IF THIS WILL WORK LETS SEE THOUGH
+    //IT WORKS WOO THATS GREAT
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
